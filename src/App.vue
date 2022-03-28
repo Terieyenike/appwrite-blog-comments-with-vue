59128ff0135174d1cc5c4178438c0cfed2770500
Oteri Eyenike <teyenike1@gmail.com>
2022-03-28 18:33:08 +0100

    Add: blog comments with vue and appwrite

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Blog />
+    <div class="comment-space">
+      <p>
+        {{
+          comments.length > 1
+            ? `Comments (${comments.length})`
+            : comments.length == 1
+            ? `Comment (${comments.length})`
+            : ''
+        }}
+      </p>
+    </div>
+    <div v-for="data in comments" :key="data.comment">
+      <comment class="message" :data="data" v-on:refreshData="fetchComments" />
+    </div>
+    <add-comment :fetchComments="fetchComments" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Blog from '@/components/Blog.vue';
+import AddComment from '@/components/AddComment.vue';
+import Comment from '@/components/Comment';
+
+import { COLLECTION_ID, db } from '@/utils';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Blog,
+    AddComment,
+    Comment,
+  },
+  created() {
+    this.fetchComments();
+  },
+  data() {
+    return {
+      comments: [],
+    };
+  },
+  methods: {
+    fetchComments() {
+      let promise = db.listDocuments(COLLECTION_ID);
+
+      promise.then(
+        (res) => {
+          // console.log(res);
+          this.comments = res.documents;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+  },
+};
 </script>
 
 <style>
+.comment-space {
+  margin-bottom: 1em;
+}
+
+.comment-space p {
+  font-weight: 700;
+  font-size: 1.5rem;
+}
+
+.container {
+  margin-inline: auto;
+  max-width: 75rem;
+  width: 85%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Reset margins */
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+p,
+figure,
+picture {
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  font-weight: 400;
+}
+
+img,
+picutre {
+  max-width: 100%;
+  display: block;
+}
+
+/* make form elements easier to work with */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+button {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="comment">
+    <div class="comment__flex">
+      <p>{{ data.comment }}</p>
+      <div class="comment__flex-btn">
+        <button class="update" @click="updateComment">Update</button>
+        <button class="del" @click="deleteComment">Delete</button>
+      </div>
+    </div>
+    <div v-if="this.open" class="open">
+      <form @submit.prevent="updateCommentMethod">
+        <div class="form-group">
+          <textarea
+            cols="50"
+            rows="10"
+            id
+            required
+            :placeholder="data.comment"
+            v-model="updateText"
+          />
+          <button class="update">Update</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from '@/utils';
+export default {
+  props: ['data'],
+  data() {
+    return {
+      open: false,
+      updateText: '',
+    };
+  },
+  methods: {
+    updateComment() {
+      this.open = !this.open;
+    },
+
+    updateCommentMethod() {
+      let promise = db.updateDocument(this.data.$collection, this.data.$id, {
+        comment: this.updateText,
+      });
+      // eslint-disable-next-line vue/no-mutating-props
+      this.data.comment = this.updateText;
+      this.open = false
+      promise.then(
+        () => {
+          this.$emit('refreshData');
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+
+    deleteComment() {
+      let promise = db.deleteDocument(this.data.$collection, this.data.$id);
+      promise.then(
+        () => {
+          this.$emit('refreshData');
+        },
+        (err) => {
+          console.log('error occured', err);
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.message {
+  margin-bottom: 2em;
+}
+.comment {
+  margin: 1em 0 2em;
+  border-bottom: 1px solid #ccc;
+}
+
+.comment__flex-btn {
+  margin: 1em 0;
+}
+
+button + button {
+  margin-left: 1em;
+}
+
+button {
+  padding: 0.75em 1em;
+  border: unset;
+}
+
+.del {
+  background: #f93943;
+  color: #000807;
+}
+
+.update {
+  background: #7eb2dd;
+  color: #000807;
+}
+
+textarea {
+  width: 100%;
+}
+
+.open {
+  margin: 1em 0;
+}
+</style>
